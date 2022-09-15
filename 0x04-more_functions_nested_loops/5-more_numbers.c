8903e337606ec965f14b54d7b0de5e8cd89c06d8
Jeremy John <newtonjohn011@gmail.com>
2022-09-15 13:22:48 -0700

    God did

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,9 @@
 #include "holberton.h"
 
 /**
- * more_numbers - print 0 to 14 ten times
+ * more_numbers - prints 10 times the numbers, from 0 - 14,
+ * followed by a new line
+ * Return: Nothint
  */
 
 void more_numbers(void)
@@ -9,11 +11,11 @@ void more_numbers(void)
 	char i;
 	int counter;
 
-	for (counter = 1; counter <= 10; counter++)
+	for (counter = 0; counter <= 9; counter++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			if (i / 10 > 0)
+			if (i / 9 > 0)
 				_putchar((i / 10) + '0');
 			_putchar(i % 10 + '0');
 		}
