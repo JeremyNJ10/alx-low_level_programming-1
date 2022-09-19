2d99da204de86f0b1f445f2c1ab0603811ac1092
Jeremy John <newtonjohn011@gmail.com>
2022-09-19 13:11:33 -0700

    God did

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - A function that convert a string to an integer.
+ * @s: An input string
+ * Return: 0
+ */
+
+int _atoi(char *s)
+{
+	int i, val, sign;
+
+	val = 0;
+	sign = 1;
+
+	for (i = 0; s[i] != '\0' && !(s[i] >= '0' && s[i] <= '9'); i++)
+	{
+		if (s[i] == '-')
+			sign = sign * -1;
+	}
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+			val = val * 10 + sign * (s[i] - '0');
+		if (val != 0 && !(s[i] >= '0' && s[i] <= '9'))
+			return (val);
+	}
+}
