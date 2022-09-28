d0383b99632f258731d87a217189cb5651bcfa8c
Jeremy John <newtonjohn011@gmail.com>
2022-09-28 16:00:50 -0700

    GOD DID

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * factorial - compute the factorial of a number
+ * @n: the number to operate on
+ *
+ * Return: the factorial of n
+ */
+
+{
+	if (n < 0)
+		return (-1);
+	return (n > 1 ? n * factorial(n - 1) : 1);
+}
